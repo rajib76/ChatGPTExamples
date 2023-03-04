704d0beb67b3f4e7fb9c378114e65bc4f211e6d3
rajib76 <rajib76@yahoo.com>
2023-03-04 15:24:41 -0800

    Initial commit

---
+++ virtual_assistant.py
@@ -0,0 +1,26 @@
+import os
+import openai
+openai.api_key= os.getenv('API_TOKEN')
+print(os.getenv('API_TOKEN'))
+
+init_context = "You are a recruiter and pre-screening " \
+               "a potential candidate for a " \
+               "data engineer position " \
+               "with Snowflake skills." \
+               "You need to ask some basic question " \
+               "to understand the candidates " \
+               "expectation and aspirations." \
+               "You need to finish after asking 5 questions " \
+               "followed by scheduing an appointment"
+
+messages = [{"role":"system", "content": init_context}]
+
+while True:
+    user_message = input("Candidate: ")
+    messages.append({"role":"user", "content":user_message})
+    chat = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo", messages=messages
+    )
+    reply = chat.choices[0].message.content
+    messages.append({"role": "assistant", "content": reply})
+    print(reply)
